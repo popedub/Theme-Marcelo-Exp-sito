b52c6b6b46d37bba67f3674e2f0f22039f8ce456
poopedub <pablo.volt@gmail.com>
2020-08-13 13:02:49 +0200

    set filter substractive

--- resources/assets/scripts/routes/blog.js
+++ resources/assets/scripts/routes/blog.js
@@ -4,11 +4,10 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
-    $(document).ready(function () {
-      $('tbody:first-child').prepend('<div class="grid-sizer"></div>');
-    })
+
     // init Isotope
     $(window).load(function(){
+
       var $grid = $('.grid').isotope({
         itemSelector: 'article',
         percentPosition: true,
@@ -22,11 +21,44 @@ export default {
       $grid.imagesLoaded().progress(function () {
         $grid.isotope('layout');
       });
+
+      // store filter for each group
+      var filters = {};
       // filter items on button click
       $('.filter-button-group').on('click', 'a', function () {
-        var filterValue = $(this).attr('data-filter');
+        // get group key
+        var $this = $(this);
+        var filterValue;
+
+        var $buttonGroup = $this.parents('.filter-button-group');
+        var filterGroup = $buttonGroup.attr('data-filter-group');
+        // set filter for group
+        filters[filterGroup] = $this.attr('data-filter');
+
+
+        if($this.is('.active')) {
+          filters[filterGroup] = '';
+          $this.removeClass('active')
+
+        } else {
+          $this.toggleClass('active').siblings().removeClass('active');
+        }
+
+
+
+        // combine filters
+        filterValue = concatValues(filters);
         $grid.isotope({ filter: filterValue });
+
       });
+      // flatten object by concatting values
+      function concatValues(obj) {
+        var value = '';
+        for (var prop in obj) {
+          value += obj[prop];
+        }
+        return value;
+      }
 
       //cuándo isotope ha renderizado los elementos, check el border-right
       $grid.on('layoutComplete',
@@ -36,7 +68,7 @@ export default {
           //queda mirar los breakpoints
           var count = 1;
           $.each(elems, function(){
-            console.log(count)
+
 
             if(count == 1 || count == 2) {
               $(this).css('border-right', '1px solid black')
--- resources/views/partials/filtro.blade.php
+++ resources/views/partials/filtro.blade.php
@@ -1,13 +1,14 @@
+<div class="btn-group-vertical button-group filter-button-group" data-filter-group="archivo" role="group">
+  <a data-filter=".category-proyectos-en-proceso" class="btn btn-link">Proyectos en proceso</a>
+  <a data-filter=".category-archivo-general" class="btn btn-link">Archivo General</a>
+</div>
+<div class="btn-group-vertical button-group filter-button-group" data-filter-group="actividades" role="group">
 
-  <div class="btn-group-vertical button-group filter-button-group" role="group">
-    <a data-filter="*" class="btn btn-link">Archivo General</a>
-    <a data-filter=".category-exposiciones" class="btn btn-link">Exposiciones</a>
-    <a data-filter=".category-escrituras" class="btn btn-link">Escrituras</a>
-    <a data-filter=".category-visualidades" class="btn btn-link">Visualidades</a>
-    <a data-filter=".category-sonoridades" class="btn btn-link">Sonoridades</a>
-    <a data-filter=".category-publicos" class="btn btn-link">Públicos</a>
-    <a data-filter=".category-pedagogias" class="btn btn-link">Pedagogías</a>
-    <a data-filter=".category-politicas" class="btn btn-link">Políticas</a>
-  </div>
-
-
+  <a data-filter=".category-exposiciones" class="btn btn-link">Exposiciones</a>
+  <a data-filter=".category-escrituras" class="btn btn-link">Escrituras</a>
+  <a data-filter=".category-visualidades" class="btn btn-link">Visualidades</a>
+  <a data-filter=".category-sonoridades" class="btn btn-link">Sonoridades</a>
+  <a data-filter=".category-publicos" class="btn btn-link">Públicos</a>
+  <a data-filter=".category-pedagogias" class="btn btn-link">Pedagogías</a>
+  <a data-filter=".category-politicas" class="btn btn-link">Políticas</a>
+</div>
